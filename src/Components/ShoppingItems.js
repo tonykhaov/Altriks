f846ac6915464b45aef1e5ce591ee71b78ff933e
Tony KHAOV <tony.khaov@gmail.com>
2020-04-08 17:48:19 +0200

    :construction: Create ShoppingItems component

---
+++ src/Components/ShoppingItems.js
@@ -0,0 +1,43 @@
+import React from "react";
+import styled from "styled-components";
+import { items } from "../api";
+import Item from "./Item";
+import Cart from "./Cart";
+
+function ShoppingItems() {
+  const itemsToDisplay = items.map((item) => (
+    <Item key={item.id} item={item} />
+  ));
+  return (
+    <>
+      <ItemsWrapper>
+        <h1>Items Available</h1>
+        <hr />
+        <ItemsContainer>{itemsToDisplay}</ItemsContainer>
+      </ItemsWrapper>
+      <Cart />
+    </>
+  );
+}
+
+const ItemsWrapper = styled.main`
+  grid-area: items;
+  padding: 24px 48px;
+  border-right: 1px solid lightgrey;
+
+  hr {
+    margin: 12px 0;
+    color: lightgrey;
+  }
+
+  @media (max-width: 768px) {
+    padding: 12px 8px;
+  }
+`;
+
+const ItemsContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(100px, 200px));
+  grid-gap: 12px;
+`;
+export default ShoppingItems;
