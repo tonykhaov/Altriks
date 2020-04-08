be3860d156f595f22e8efeecdc60b75ddc66caee
Tony KHAOV <tony.khaov@gmail.com>
2020-04-08 17:46:53 +0200

    :pencil: Update items on api

---
+++ src/api.js
@@ -0,0 +1,44 @@
+export const items = [
+  {
+    id: "bike__1",
+    title: "Bike",
+    thumbnail_url: "/bike.jpg",
+    price: 160,
+    __typename: "Item",
+  },
+  {
+    id: "helmet__1",
+    title: "Helmet",
+    thumbnail_url: "/helmet.jpg",
+    price: 39,
+    __typename: "Item",
+  },
+  {
+    id: "tshirt__1",
+    title: "T-shirt",
+    thumbnail_url: "/tshirt.jpg",
+    price: 17,
+    __typename: "Item",
+  },
+  {
+    id: "backpack__1",
+    title: "Backpack",
+    thumbnail_url: "/backpack.jpg",
+    price: 57,
+    __typename: "Item",
+  },
+  {
+    id: "pant__1",
+    title: "Pant",
+    thumbnail_url: "/pant.jpg",
+    price: 37,
+    __typename: "Item",
+  },
+  {
+    id: "gloves__1",
+    title: "Gloves",
+    thumbnail_url: "/gloves.jpg",
+    price: 7,
+    __typename: "Item",
+  },
+];
