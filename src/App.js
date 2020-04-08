81073289b8823ec6b12a4b8dbc3402634ab0f5bc
Tony KHAOV <tony.khaov@gmail.com>
2020-04-08 17:47:07 +0200

    :building_construction: Making architectural changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
 import React from "react";
+import Header from "./Components/Header";
+import ShoppingItems from "./Components/ShoppingItems";
+import Footer from "./Components/Footer";
+import styled from "styled-components";
 
 function App() {
   return (
-    <div>
-      <header>
-        <h1>E-commerce website with React & Apollo (state manager)</h1>
-      </header>
-      <main></main>
-      <footer></footer>
-    </div>
+    <StoreWrapper>
+      <Header />
+      <ShoppingItems />
+      <Footer />
+    </StoreWrapper>
   );
 }
+
+const StoreWrapper = styled.div`
+  min-height: 100vh;
+  display: grid;
+  grid-template-columns: 3fr 1fr;
+  grid-template-rows: auto 1fr auto;
+  grid-template-areas:
+    "header header"
+    "items cart"
+    "footer footer";
+  @media (max-width: 1024px) {
+    grid-template-columns: 2fr 1fr;
+  }
+  @media (max-width: 768px) {
+    grid-template-columns: 1fr 1fr;
+  }
+`;
 
 export default App;
