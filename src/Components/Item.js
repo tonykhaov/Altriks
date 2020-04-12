4a916d2641cd3db39f0efccbe50c52f636345eb7
Tony KHAOV <tony.khaov@gmail.com>
2020-04-12 14:18:10 +0200

    :rocket: Connect Redux to this component

--- src/Components/Item.js
+++ src/Components/Item.js
@@ -1,15 +1,32 @@
 import React from "react";
 import styled from "styled-components";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
 
-function Item({ item, className }) {
+import addToCart from "../Redux/actionCart";
+
+function Item({ item, className, index, dispatch }) {
+  const { title, price, thumbnail_url } = item;
   return (
     <div className={className}>
-      <img src={item.thumbnail_url} alt={item.title} />
-      <h1>{item.title}</h1>
-      <p>{item.price}</p>
-      <button>Add to cart</button>
+      <img src={thumbnail_url} alt={title} />
+      <h1>{title}</h1>
+      <p>{price}</p>
+      <button onClick={() => dispatch({ type: "ADD_TO_CART", id: index })}>
+        Add to cart
+      </button>
     </div>
   );
 }
 
-export default styled(Item)``;
+const StyledItem = styled(Item)``;
+
+// const mapDispatchToProps = (dispatch) =>
+//   bindActionCreators(
+//     {
+//       addToCart,
+//     },
+//     dispatch
+//   );
+
+export default connect()(StyledItem);
