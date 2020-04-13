780e84558cbca29f0f5a9d34d9b37d054be91cf2
Tony KHAOV <tony.khaov@gmail.com>
2020-04-13 12:27:00 +0200

    :rocket: Add social medias

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,8 +4,18 @@ import styled from "styled-components";
 function Footer({ className }) {
   return (
     <footer className={className}>
-      <h6>made by Tony KHAOV</h6>
-      <p>I don't own the images, they all belong to Alltricks.</p>
+      <h6>JOIN THE ALTRIKS PELOTON</h6>
+      <div className="social-medias">
+        <a href="https://twitter.com/Alltricks_com">
+          <i className="fab fa-twitter"></i>
+        </a>
+        <a href="https://www.facebook.com/alltricks">
+          <i className="fab fa-facebook-f"></i>
+        </a>
+        <a href="https://www.instagram.com/alltricks_com/">
+          <i className="fab fa-instagram"></i>
+        </a>
+      </div>
     </footer>
   );
 }
@@ -13,15 +23,27 @@ function Footer({ className }) {
 export default styled(Footer)`
   grid-area: footer;
   text-align: center;
-  padding: 16px;
+  padding: 12px 0 6px;
   background: #222;
+  color: white;
+
   h6 {
-    font-size: 16px;
-    font-weight: normal;
-    color: white;
+    font-size: 14px;
   }
 
-  p {
+  .social-medias {
+    display: flex;
+    justify-content: space-around;
+    margin: 0 auto;
+    max-width: 192px;
+    margin-top: 12px;
+  }
+
+  .fab {
+    font-size: 24px;
+  }
+
+  .social-medias a:hover {
     color: grey;
   }
 `;
