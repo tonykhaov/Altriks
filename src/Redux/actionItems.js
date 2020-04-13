f4fb225a9cd6ab9f3687b2d3da2feec57444f14a
Tony KHAOV <tony.khaov@gmail.com>
2020-04-13 15:45:40 +0200

    :wrench: Better debuger

--- src/Redux/actionItems.js
+++ src/Redux/actionItems.js
@@ -1,9 +1,11 @@
 import { items } from "../api";
 
+export const GET_ITEMS = "GET_ITEMS";
+
 export function getItems() {
   return function (dispatch) {
     return dispatch({
-      type: "GET_ITEMS",
+      type: GET_ITEMS,
       data: items,
     });
   };
--- src/Redux/reducerItems.js
+++ src/Redux/reducerItems.js
@@ -1,10 +1,12 @@
+import { GET_ITEMS } from "./actionItems";
+
 const initialState = {
   items: [],
 };
 
 export default function (state = initialState, action) {
   switch (action.type) {
-    case "GET_ITEMS":
+    case GET_ITEMS:
       return { ...state, items: action.data };
     default:
       return state;
