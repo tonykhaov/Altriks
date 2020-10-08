6a432cb29eb20e6c7bc7de85ceeb84e1ab644c15
Tony Khaov <tony.khaov@gmail.com>
2020-10-08 16:32:37 +0200

    Write first E2E test (change currency)

--- cypress/e2e/appAltriks.js
+++ cypress/e2e/appAltriks.js
@@ -1,6 +1,11 @@
 describe("E2E Test Altriks App", () => {
   it("can change currency", () => {
     const user = cy;
-    user.visit("/");
+    user
+      .visit("/")
+      .get(".sc-AxgMl > :nth-child(2)")
+      .click()
+      .get(".sc-AxgMl > :nth-child(1)")
+      .click();
   });
 });
