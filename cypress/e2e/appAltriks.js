31fd5f8531400b7aba6a63bfe0cdb05a95244616
Tony Khaov <tony.khaov@gmail.com>
2020-10-13 20:25:00 +0200

    Write 'can add items and checkout' test

--- cypress/e2e/appAltriks.js
+++ cypress/e2e/appAltriks.js
@@ -1,20 +1,30 @@
 describe("E2E Test Altriks App", () => {
-  it("can see nothing in the cart message", () => {
+  it("can see nothing in the cart message and checkout should be disabled", () => {
     cy.visit("/");
     cy.findByAltText(/empty cart spinning cog/i).should("exist");
+    cy.findByText(/checkout/i).should("be.disabled");
   });
 
   it("can add item in the cart", () => {
+    cy.visit("/");
     cy.findByTestId("BikeInCart").should("not.exist");
     cy.findByTestId("BikeAddToCartButton").click().click().click();
     cy.findByTestId("BikeInCart").should("exist");
   });
 
-  it("can change currency", () => {
-    cy.findByRole("button", { name: /.* usd/i }).click();
+  it("can add items and checkout", () => {
+    cy.visit("/");
+    cy.findByTestId("BikeInCart").should("not.exist");
+    cy.findByTestId("BikeAddToCartButton").click().click().click();
+    cy.findByTestId("BikeInCart").should("exist");
 
+    cy.findByText(/checkout/i).click();
+    cy.findByAltText(/empty cart spinning cog/i).should("exist");
+  });
+
+  it("can change currency", () => {
+    cy.visit("/");
+    cy.findByRole("button", { name: /.* usd/i }).click();
     cy.findByRole("button", { name: /.* eur/i }).click();
   });
 });
-
-console.log(cy);
